38f67b59e5a148ad23946656167aef770eb8b0a2
PropellerheadViJ <dr.vij@icloud.com>
2025-10-06 22:14:12 +0200

    combined all together

---
+++ Packages/Sangria/SourceGenerators/AsmdefHelper.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// We need this because Source Generator DLL only will not be compiled
+/// </summary>
+public static class AsmdefHelper
+{
+}
