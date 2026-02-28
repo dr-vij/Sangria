c80412b22c5b3e44a15080b9e0e3fa23e50a0339
PropellerheadViJ <dr.vij@icloud.com>
2026-03-01 00:26:10 +0100

    Some path fixes and documentation

--- Packages/Sangria/SangriaTexture/Scripts/MaterialProvider.cs
+++ Packages/Sangria/SangriaTexture/Scripts/MaterialProvider.cs
@@ -1,5 +1,5 @@
 using System;
-using UTools.SourceGeneratorAttributes;
+using Sangria.SourceGeneratorAttributes;
 
 namespace ViJApps.CanvasTexture
 {
---
+++ SangriaUP/Assets/Examples/SG/SgExample.cs
@@ -0,0 +1,7 @@
+using Sangria.SourceGeneratorAttributes;
+using UnityEngine;
+
+public partial class SgExample : MonoBehaviour
+{
+    [PropertySubscription] private bool m_IsTrue;
+}
